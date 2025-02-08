6b21b1f91aa55b54b3ce169740c4ceb639e13f88
Gwen-83 <158830850+Gwen-83@users.noreply.github.com>
2025-02-08 18:59:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -179,5 +179,4 @@ if __name__ == '__main__':
     serial_thread = Thread(target=read_serial_data)
     serial_thread.daemon = True
     serial_thread.start()
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
